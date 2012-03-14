35086e0b3e04939fafda540e1ee21d07781a456e
Akodiat <Akodiat@129.16.182.158>
2012-03-14 18:59:40 +0100

    fdrg

--- Fallout-Equestria-The-Game/src/DisplayTest.java
+++ Fallout-Equestria-The-Game/src/DisplayTest.java
@@ -1,7 +1,7 @@
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
-
+//Akodiat was here
 public class DisplayTest {
 	
 	public static void main(String[] args) throws LWJGLException {
