239651652b750e9857da1e44c6d2bff83ef6cc91
Lukas Kurtyan <kurtyan@student.chalmers.se>
2012-03-15 09:07:24 +0100

    First test class

---
+++ Fallout-Equestria-The-Game/src/tests/Vector2Tests.java
@@ -0,0 +1,29 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import math.Vector2;
+
+import org.junit.Test;
+
+public class Vector2Tests {
+	
+	
+	
+	@Test
+	public void testAddition() {
+	
+		float x1 = (float)Math.random() * 10;
+		float x2 = (float)Math.random() * 10;
+		float y1 = (float)Math.random() * 10;
+		float y2 = (float)Math.random() * 10;
+		
+		Vector2 vector0 = new Vector2(x1,y1);
+		Vector2 vector1 = new Vector2(x2, y2);
+		
+		Vector2 result = Vector2.add(vector0, vector1);
+		
+		assertTrue(result.X == x1 + x2);
+		assertTrue(result.Y == y1 + y2);
+	}
+}
