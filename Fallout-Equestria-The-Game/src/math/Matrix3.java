b45c0b6b6b4a60b513a09b8f9630b5c1cba7cd0c
Pontus Pall <pallp@student.chalmers.se>
2012-03-16 11:03:29 +0100

    Matrix3-funktioner

--- Fallout-Equestria-The-Game/src/math/Matrix3.java
+++ Fallout-Equestria-The-Game/src/math/Matrix3.java
@@ -2,17 +2,9 @@ package math;
 
 public final class Matrix3 {
 
-	public final float m00;
-	public final float m01;
-	public final float m02;
-
-	public final float m10;
-	public final float m11;
-	public final float m12;
-
-	public final float m20;
-	public final float m21;
-	public final float m22;
+	public final float a; public final float b; public final float c;
+	public final float d; public final float e; public final float f;
+	public final float g; public final float h; public final float i;
 
 	/**Creates a 3x3 Identity Matrix.
 	 * 
@@ -39,9 +31,9 @@ public final class Matrix3 {
 			float m10, float m11, float m12,
 			float m20, float m21, float m22) {
 
-		this.m00 = m00; this.m01 = m01; this.m02 = m02;
-		this.m10 = m10; this.m11 = m11; this.m12 = m12;
-		this.m20 = m20; this.m21 = m21; this.m22 = m22;
+		this.a = m00; this.d = m01; this.g = m02;
+		this.b = m10; this.e = m11; this.h = m12;
+		this.c = m20; this.f = m21; this.i = m22;
 	}
 
 	/**Creates a 3x3 custom matrix.
@@ -53,18 +45,17 @@ public final class Matrix3 {
 		if(data.length != 9) {
 			throw new IllegalArgumentException();
 		}
-		m00 = data[0];
-		m10 = data[1];
-		m20 = data[2];
+		a = data[0];
+		b = data[1];
+		c = data[2];
 
-		m01 = data[3];
-		m11 = data[4];
-		m21 = data[5];
+		d = data[3];
+		e = data[4];
+		f = data[5];
 
-		m02 = data[6];
-		m12 = data[7];
-		m22 = data[8];
-
+		g = data[6];
+		h = data[7];
+		i = data[8];
 	}
 
 	/**Gets the transpose of the matrix.
@@ -72,9 +63,49 @@ public final class Matrix3 {
 	 * @return the transpose of the matrix.
 	 */
 	public Matrix3 transpose() {
-		return new Matrix3(m00, m10, m20,
-				m01, m11, m21,
-				m02, m12, m22);
+		return new Matrix3(a, b, c,
+				d, e, f,
+				g, h, i);
 	}
+
+	public Matrix3 mul(float k, Matrix3 m) {
+		return new Matrix3(k*m.a, k*m.d, k*m.g,
+				k*m.b, k*m.e, k*m.h,
+				k*m.c, k*m.f, k*m.i);
+	}
+
+	public Matrix3 rotate(float angle) {
+		float cosAngle = (float)Math.cos(angle);
+		float sinAngle = (float)Math.sin(angle);
+
+		return new Matrix3(cosAngle, -sinAngle, 0, 
+				sinAngle, cosAngle,  0, 
+				0,        0,         1);
+	}
+
+	public static Matrix3 createScale(float scale) {
+		return new Matrix3(scale, 0, 0,
+				0, scale, 0,
+				0, 0,     1);
+	}
+
+	public static Matrix3 mul(Matrix3 l, Matrix3 r) {
+
+		float m00 = l.a * r.a + l.d * r.b + l.g * r.c;
+		float m01 = l.a * r.d + l.d * r.e + l.g * r.f;
+		float m02 = l.a * r.g + l.d * r.h + l.g * r.i;
+
+		float m10 = l.b * r.a + l.e * r.b + l.h * r.c;
+		float m11 = l.b * r.d + l.e * r.e + l.h * r.f;
+		float m12 = l.b * r.g + l.e * r.h + l.h * r.i;
+
+		float m20 = l.c * r.a + l.f * r.b + l.i * r.c;
+		float m21 = l.c * r.d + l.f * r.e + l.i * r.f;
+		float m22 = l.c * r.g + l.f * r.h + l.i * r.i;
+		
+		return new Matrix3(m00, m01, m02,
+				           m10, m11, m12,
+				           m20, m21, m22);
+	}	
 
 }
