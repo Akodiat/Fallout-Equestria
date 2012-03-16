5fdb2044687ce20f1b0b9821d2e86a9e1859f6dc
Gustav Alm Rosenblad <agustav@student.chalmers.se>
2012-03-16 10:40:26 +0100

    Circle finns NU.

---
+++ Fallout-Equestria-The-Game/src/tests/CircleTests.java
@@ -0,0 +1,31 @@
+package tests;
+
+import math.Vector2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * 
+ * @author Gustav
+ *
+ */
+public class CircleTests {
+
+	private float x1 = (float)Math.random() * 10;
+	private float x2 = (float)Math.random() * 10;
+	private float y1 = (float)Math.random() * 10;
+	private float y2 = (float)Math.random() * 10;
+	
+	private float x1 = (float)Math.random() * 10;
+	private float x2 = (float)Math.random() * 10;
+	
+	private Vector2 vector1 = new Vector2(x1, y1);
+	private Vector2 vector2 = new Vector2(x2, y2);
+	
+	@Test
+	public void testDistanceSquared(){
+		
+	}
+}
---
+++ Fallout-Equestria-The-Game/src/utils/Circle.java
@@ -0,0 +1,39 @@
+package utils;
+
+import math.Vector2;
+
+public class Circle {
+
+	private Vector2 position;
+	private float radius;
+
+	public Circle(Vector2 position, float radius) {
+		this.position = position;
+		this.radius = radius;
+	}
+	
+	public Vector2 getPosition() {
+		return position;
+	}
+	public void setPosition(Vector2 position) {
+		this.position = position;
+	}
+	public float getRadius() {
+		return radius;
+	}
+	public void setRadius(float radius) {
+		this.radius = radius;
+	}
+
+	public Boolean intersects(Circle other){
+		float comboRadius = this.radius + other.getRadius();
+		return comboRadius*comboRadius>distanceSquared(other); //Don't need no sqrt. This is perfectly sufficient.
+	}
+	
+	
+	private float distanceSquared(Circle other){
+		float dx = this.position.X - other.position.X;
+		float dy = this.position.Y - other.position.Y;
+		return (dx*dx + dy*dy);
+	}
+}
