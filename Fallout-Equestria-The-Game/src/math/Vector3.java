41a2ba3a5dcfbed20d74a910a344bbfcb09c7242
Pontus Pall <pallp@student.chalmers.se>
2012-03-16 10:35:35 +0100

    Vector3 class created

---
+++ Fallout-Equestria-The-Game/src/math/Vector3.java
@@ -0,0 +1,13 @@
+package math;
+
+public class Vector3 {
+	public final float X;
+	public final float Y;
+	public final float Z;
+	
+	public Vector3(float x, float y, float z) {
+		this.X = x;
+		this.Y = y;
+		this.Z = z;
+	}
+}
