9f644b57b368e4cd3a488edc9f80fc9824e7f87f
Akodiat <Akodiat@129.16.187.119>
2012-03-21 11:11:30 +0100

    SimpleShootingTest made

--- Fallout-Equestria-The-Game/src/tests/SimpleShootingTest.java
+++ Fallout-Equestria-The-Game/src/tests/SimpleShootingTest.java
@@ -1,12 +1,28 @@
 package tests;
 
+import java.io.IOException;
+
+import org.lwjgl.LWJGLException;
+
+import components.InputComponent;
+import components.PhysicsComponent;
+import components.RenderingComponent;
+
+import entityFramework.IEntity;
 import entityFramework.IEntityManager;
+import entitySystems.AttackResolveSystem;
+import entitySystems.BasicRenderingSystem;
+import entitySystems.CharacterControllerSystem;
+import entitySystems.InputSystem;
+import entitySystems.PhysicsSystem;
+import graphics.Color;
+import graphics.TextureLoader;
 import utils.Rectangle;
 
 public class SimpleShootingTest extends AbstractSystemTest{
 	
-	public static void main(String[] args){
-		
+	public static void main(String[] args) throws LWJGLException{
+		new SimpleShootingTest(new Rectangle(0,0, 1366, 768), true).start();
 	}
 
 	public SimpleShootingTest(Rectangle screenDim, boolean fullScreen) {
@@ -15,14 +31,33 @@ public class SimpleShootingTest extends AbstractSystemTest{
 
 	@Override
 	public void initializeSystems() {
-		// TODO Auto-generated method stub
-		
+		tester.addLogicubSystem(new CharacterControllerSystem(this.tester.getWorld()));
+		tester.addLogicubSystem(new InputSystem(this.tester.getWorld()));
+		tester.addLogicubSystem(new PhysicsSystem(this.tester.getWorld()));
+		tester.addRenderSubSystem(new BasicRenderingSystem(this.tester.getWorld(), this.graphics));
 	}
 
 	@Override
 	public void initializeEntities(IEntityManager manager) {
-		// TODO Auto-generated method stub
+		IEntity player = manager.createEmptyEntity();
 		
+		PhysicsComponent physComp = new PhysicsComponent();
+		InputComponent inpComp = new InputComponent();
+		RenderingComponent rendComp = new RenderingComponent();
+		
+		rendComp.setColor(new Color(42,200,255, 255));
+		try {
+			rendComp.setTexture(TextureLoader.loadTexture(TextureTest.class.getResourceAsStream("PPieLauncher.png")));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		player.addComponent(rendComp);
+		player.addComponent(inpComp);
+		player.addComponent(physComp);
+		
+		player.refresh();
 	}
 
 }
