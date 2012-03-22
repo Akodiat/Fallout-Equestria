72f793e6cd542e99d1c9e162d924775cd7999149
Lukas Kurtyan <kurtyan@student.chalmers.se>
2012-03-22 15:49:54 +0100

    Fixed the hud drawing

--- Fallout-Equestria-The-Game/src/entitySystems/HUDRenderingSystem.java
+++ Fallout-Equestria-The-Game/src/entitySystems/HUDRenderingSystem.java
@@ -3,12 +3,14 @@ package entitySystems;
 import utils.Rectangle;
 import components.HealthComponent;
 
+import math.Matrix4;
 import math.Vector2;
 import entityFramework.ComponentMapper;
 import entityFramework.EntitySystem;
 import entityFramework.IEntity;
 import entityFramework.IEntityWorld;
 import graphics.Color;
+import graphics.ShaderEffect;
 import graphics.SpriteBatch;
 import graphics.Texture2D;
 
@@ -39,9 +41,20 @@ public class HUDRenderingSystem extends EntitySystem {
 		HealthComponent comp = this.healthCM.getComponent(entity);
 		float ratio = comp.getHealthPoints() / comp.getMaxHealth();
 		
+		this.batch.end();
+		ShaderEffect effect = this.batch.getActiveEffect();
+		Matrix4 view = this.batch.getView();
+		
+		this.batch.begin(effect, Matrix4.Identity);
+
 		Rectangle rect = new Rectangle((int)this.healthPos.X, (int)this.healthPos.Y, (int)(100.0f * ratio), 20);
 		this.batch.draw(Texture2D.getPixel(), rect, Color.White, null);
 		
+		this.batch.end();
+		this.batch.begin(effect, view);
+
+		
+	
 	}
 
 }
