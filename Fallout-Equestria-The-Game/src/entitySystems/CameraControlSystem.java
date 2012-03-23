b6ebf43e793c2537fb19e37e4caba73702046853
Lukas Kurtyan <kurtyan@student.chalmers.se>
2012-03-23 16:30:08 +0100

    Added a camera system

---
+++ Fallout-Equestria-The-Game/src/entitySystems/CameraControlSystem.java
@@ -0,0 +1,51 @@
+package entitySystems;
+
+import math.Vector2;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+
+import components.TransformationComp;
+
+import utils.Camera2D;
+import entityFramework.ComponentMapper;
+import entityFramework.EntitySystem;
+import entityFramework.IComponent;
+import entityFramework.IEntity;
+import entityFramework.IEntityWorld;
+
+public class CameraControlSystem extends EntitySystem{
+
+	private final Camera2D camera;
+	
+	protected CameraControlSystem(IEntityWorld world,Camera2D camera) {
+		super(world);
+		this.camera = camera;
+	}
+	
+	public ComponentMapper<TransformationComp> transCM;
+
+	@Override
+	public void initialize() {
+		transCM = ComponentMapper.create(this.getWorld().getDatabase(), TransformationComp.class);
+	}
+
+	@Override
+	public void process() {
+		ImmutableSet<IEntity> cameraTargets = this.getWorld().getEntityManager().getEntityGroup("CAMERA_TARGET");
+		if(cameraTargets != null) {
+			float x = 0, y = 0;
+			for (IEntity entity : cameraTargets) {
+				TransformationComp transformation = this.transCM.getComponent(entity);
+				x += transformation.getPosition().X;
+				y += transformation.getPosition().Y;
+			}
+			
+			x /= cameraTargets.size();
+			y /= cameraTargets.size();
+			
+			camera.setPosition(new Vector2(x, y));
+		}
+	}
+
+}
