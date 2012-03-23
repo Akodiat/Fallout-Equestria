b01a7dfc72a137a794215f0971b49c786cc81b0f
Lukas Kurtyan <kurtyan@student.chalmers.se>
2012-03-23 10:57:58 +0100

    Added ability to see ability points

--- Fallout-Equestria-The-Game/src/entitySystems/HUDRenderingSystem.java
+++ Fallout-Equestria-The-Game/src/entitySystems/HUDRenderingSystem.java
@@ -1,6 +1,7 @@
 package entitySystems;
 
 import utils.Rectangle;
+import components.ActionPointsComponent;
 import components.HealthComponent;
 
 import math.Matrix4;
@@ -19,6 +20,7 @@ public class HUDRenderingSystem extends EntitySystem {
 	private final String playerLabel;
 	private SpriteBatch batch;
 	private Vector2 healthPos;
+	private Vector2 apPos;
 
 	public HUDRenderingSystem(IEntityWorld world, SpriteBatch batch,
 			String playerLabel) {
@@ -26,14 +28,20 @@ public class HUDRenderingSystem extends EntitySystem {
 		this.batch = batch;
 		this.playerLabel = playerLabel;
 		this.healthPos = new Vector2(100, 100);
+		this.apPos = new Vector2(100,140);
 	}
 
 	private ComponentMapper<HealthComponent> healthCM;
+	private ComponentMapper<ActionPointsComponent> apCM;
+	
 
 	@Override
 	public void initialize() {
 		healthCM = ComponentMapper.create(this.getWorld().getDatabase(),
 				HealthComponent.class);
+		apCM = ComponentMapper.create(this.getWorld().getDatabase(),
+				ActionPointsComponent.class);
+		
 
 	}
 
@@ -54,12 +62,24 @@ public class HUDRenderingSystem extends EntitySystem {
 
 			this.batch.begin(effect, Matrix4.Identity);
 
-			Rectangle rect = new Rectangle((int) this.healthPos.X,
+			Rectangle rectHealth = new Rectangle((int) this.healthPos.X,
 					(int) this.healthPos.Y, (int) (100.0f * ratio), 20);
-			this.batch.draw(Texture2D.getPixel(), rect, Color.White, null);
+			this.batch.draw(Texture2D.getPixel(), rectHealth, Color.White, null);
 
+			ActionPointsComponent actionComp = this.apCM.getComponent(entity);
+			ratio = actionComp.getAbilityPoints() / actionComp.getMaxAbilityPoints();
+			
+			Rectangle rectAp = new Rectangle((int) this.apPos.X,
+					(int) this.apPos.Y, (int) (100.0f * ratio), 20);
+			this.batch.draw(Texture2D.getPixel(), rectAp, Color.Gold, null);
+			
+			
+			
 			this.batch.end();
 			this.batch.begin(effect, view);
+			
+			
+			
 
 		}
 
