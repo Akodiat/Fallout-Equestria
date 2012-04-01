a45a9bfcc849ddabbafc8384e37dcc5693213ba4
Gustav Alm Rosenblad <agustav@student.chalmers.se>
2012-04-01 23:37:00 +0200

    Added the assetpanel

---
+++ Fallout-Equestria-The-Game/src/archetypeEditor/AudioPlayerFrame.java
@@ -0,0 +1,62 @@
+package archetypeEditor;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+import org.newdawn.slick.openal.Audio;
+
+import content.ContentManager;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class AudioPlayerFrame extends JFrame {
+
+	private JPanel contentPane;
+private Audio sound;
+	/**
+	 * Launch the application.
+	 */
+	/*public static void main(final String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AudioPlayerFrame frame = new AudioPlayerFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}*/
+
+	/**
+	 * Create the frame.
+	 */
+	public AudioPlayerFrame(final Audio sound) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		this.sound = sound;
+		
+		
+		
+		JButton btnNewButton = new JButton("PLAY AUDIO");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				sound.playAsSoundEffect(1.0f, 0.5f, false);
+			}
+		});
+		contentPane.add(btnNewButton, BorderLayout.CENTER);
+	}
+
+}
