a99afad45819edec8c9a39ad752396a195b9aba9
Lukas Kurtyan <kurtyan@student.chalmers.se>
2012-04-16 07:11:19 +0200

    Added some usefull methods to MathHelper

--- Fallout-Equestria-The-Game/src/math/MathHelper.java
+++ Fallout-Equestria-The-Game/src/math/MathHelper.java
@@ -8,8 +8,7 @@ public class MathHelper {
 	public final static float TauOver2  = 3.1415927f;
 	public final static float TauOver4  = 1.5707963f;
 	public final static float TauOver6  = 1.0471976f;
-	public final static float TauOver8  = 0.7853982f;
-	
+	public final static float TauOver8  = 0.7853982f;	
 	
 	public static double clamp(double min, double max, double value) {
 		if(min > max)
@@ -24,13 +23,11 @@ public class MathHelper {
 		
 		return value;	
 	}
-
 	public static float clamp(float min, float max, float value) {
 		return (float)clamp((double)min,(double)max,(double)value);
 	}
-	
 	public static long clamp(long min, long max, long value) {
-		//We annot use the double overload since precision might be lost.
+		//We cannot use the double overload since precision might be lost.
 		if(min > max)
 			throw new IllegalArgumentException("Min cannot be greater then Max");
 		
@@ -43,9 +40,51 @@ public class MathHelper {
 		
 		return value;	
 	}
-	
-	
 	public static int clamp(int min, int max, int value) {
 		return (int)clamp((long)min,(long)max,(long)value);
+	}
+	
+	public static double min(double val0, double val1) {
+		if(val0 < val1)
+			return val0;
+		else
+			return val1;
+	}
+	public static float min(float val0, float val1) {
+		return (float) min((double)val0,(double)val1);
+	}
+	public static long min(long val0,long val1) {
+		//Cannot use the double overload since we lose precision.
+		if(val0 < val1)
+			return val0;
+		else
+			return val1;
+	}
+	public static int min(int val0, int val1) {
+		return (int)min((long)val0,(long)val1);
+	}
+
+	public static double max(double val0, double val1) {
+		if(val0 > val1)
+			return val0;
+		else
+			return val1;
+	}
+	public static float max(float val0, float val1) {
+		return (float) max((double)val0,(double)val1);
+	}
+	public static long max(long val0,long val1) {
+		//Cannot use the double overload since we lose precision.
+		if(val0 > val1)
+			return val0;
+		else
+			return val1;
+	}
+	public static int max(int val0, int val1) {
+		return (int)max((long)val0,(long)val1);
+	}
+
+	public static float sin(double angle) {
+		return (float)Math.sin(angle);
 	}
 }
