e7b77118a0035b01a2e9f6e3d30c144dc6852044
Gustav Alm Rosenblad <agustav@student.chalmers.se>
2012-05-18 11:04:55 +0200

    You can now change color on your OC.

--- Fallout-Equestria-The-Game/src/screenCore/PonyCreatorScreen.java
+++ Fallout-Equestria-The-Game/src/screenCore/PonyCreatorScreen.java
@@ -1,29 +1,30 @@
 package screenCore;
 
 import animation.AnimationPlayer;
-import animation.Bones;
 import animation.PonyColorChangeHelper;
 import animation.TextureDictionary;
 
 import common.PlayerCharacteristics;
 
-import GUI.GUIFocusManager;
+import GUI.ScrollEventArgs;
 import GUI.controls.Button;
+import GUI.controls.ImageBox;
+import GUI.controls.Slider;
+import GUI.controls.Spinner;
 import GUI.controls.Textfield;
 import GUI.graphics.GUIRenderingContext;
 import GUI.graphics.LookAndFeel;
-import math.Point2;
 import math.Vector2;
 import misc.EventArgs;
 import misc.IEventListener;
 import utils.GameTime;
-import utils.Keyboard;
-import utils.Mouse;
+import utils.Rectangle;
 import utils.TimeSpan;
 import content.ContentManager;
 import graphics.Color;
 import graphics.ShaderEffect;
 import graphics.SpriteBatch;
+import graphics.Texture2D;
 
 
 public class PonyCreatorScreen extends TransitioningGUIScreen {
@@ -38,59 +39,231 @@ public class PonyCreatorScreen extends TransitioningGUIScreen {
 	private ContentManager contentManager = new ContentManager("resources");
 	private AnimationPlayer pony;
 	private PlayerCharacteristics character;
-
+	TextureDictionary assetDictionary = this.contentManager.load("rddict.tdict", TextureDictionary.class);
+	private ImageBox bG;
+	
+	private Slider bodyRedSlider;
+	private Slider bodyGreenSlider;
+	private Slider bodyBlueSlider;
+	
+	private Slider eyeRedSlider;
+	private Slider eyeGreenSlider;
+	private Slider eyeBlueSlider;
+	
+	private Slider maneRedSlider;
+	private Slider maneGreenSlider;
+	private Slider maneBlueSlider;
+	
+	private Spinner maneStyleSpinner;
 
 	public PonyCreatorScreen(String lookAndFeelPath) {
-		super(false, TimeSpan.Zero, TimeSpan.Zero, lookAndFeelPath);
+		super(false, TimeSpan.fromSeconds(1d), TimeSpan.fromSeconds(0.5d), lookAndFeelPath);
 	}
 
 	@Override
 	public void initialize(ContentManager contentManager) {
 		super.initialize(contentManager);
 		
+		this.bG = new ImageBox();
+		this.bG.setImage(contentManager.load("darkPonyville.png", Texture2D.class));
+		this.bG.setBounds(this.ScreenManager.getViewport());
+		super.addGuiControl(this.bG, new Vector2(0,this.ScreenManager.getViewport().Height), new Vector2(0,0), 
+								new Vector2(0,this.ScreenManager.getViewport().Height));
 		addPony();
-		
-		this.button = new Button();
-		this.button.setText("Do stuff");
-		this.button.setBounds(0,710,200,50);
-		new GUIFocusManager(button);
-		super.addGuiControl(button, new Vector2(10,50), new Vector2(200,50), new Vector2(10, 100));
-
-
-		this.button2 = new Button();
-		this.button2.setText("Do other stuff?");
-		this.button2.setBounds(250,710,200,50);
-		super.addGuiControl(button2, new Vector2(-200, 100), new Vector2(200,100), new Vector2(200,50));
 
 		this.textfield = new Textfield();
+		this.textfield.setBounds(0,0,250,25);
 		this.textfield.setText("");
-		this.textfield.setBounds(100,100,300,30);
-		this.textfield.setFont(this.contentManager.loadFont("Andale Mono20.xml"));
+		this.textfield.setFont(contentManager.loadFont("Andale Mono20.xml"));
 		this.textfield.setFgColor(Color.White);
 		this.textfield.setMaxLength(25);
-		super.addGuiControl(this.textfield, new Vector2(-200, 100), new Vector2(200,50), new Vector2(-200, 100));
+		super.addGuiControl(this.textfield, new Vector2(-100,this.ScreenManager.getViewport().Height), new Vector2(100,25), 
+				new Vector2(0,this.ScreenManager.getViewport().Height));
 		
+		this.bodyRedSlider = new Slider();
+		this.bodyRedSlider.setFgColor(new Color(255,50,50,255));
+		this.bodyRedSlider.setBounds(0, 0, 200, 30);
+		super.addGuiControl(this.bodyRedSlider, new Vector2(-100,this.ScreenManager.getViewport().Height), new Vector2(100,100), 
+				new Vector2(0,this.ScreenManager.getViewport().Height));
+		this.bodyRedSlider.setBounds(new Rectangle(0,0,250,30));
+		this.bodyRedSlider.setScrollMax(255);
+		this.bodyRedSlider.setScrollValue(255);
+		this.bodyRedSlider.setHorizontal(true);
+		this.bodyRedSlider.addScrollListener(new IEventListener<ScrollEventArgs>() {
+			
+			@Override
+			public void onEvent(Object sender, ScrollEventArgs e) {
+				setBodyColor();
+			}
+		});
+		this.bodyGreenSlider = new Slider();
+		this.bodyGreenSlider.setBounds(0, 0, 200, 30);
+		this.bodyGreenSlider.setFgColor(new Color(50,255,50,255));
+		super.addGuiControl(this.bodyGreenSlider, new Vector2(-100,this.ScreenManager.getViewport().Height), new Vector2(100,150), 
+				new Vector2(0,this.ScreenManager.getViewport().Height));
+		this.bodyGreenSlider.setBounds(new Rectangle(0,0,250,30));
+		this.bodyGreenSlider.setScrollMax(255);
+		this.bodyGreenSlider.setScrollValue(255);
+		this.bodyGreenSlider.setHorizontal(true);
+		this.bodyGreenSlider.addScrollListener(new IEventListener<ScrollEventArgs>() {
+			
+			@Override
+			public void onEvent(Object sender, ScrollEventArgs e) {
+				setBodyColor();
+			}
+		});
+		this.bodyBlueSlider = new Slider();
+		this.bodyBlueSlider.setBounds(0, 0, 200, 30);
+		this.bodyBlueSlider.setFgColor(new Color(50,50,255,255));
+		super.addGuiControl(this.bodyBlueSlider, new Vector2(-100,this.ScreenManager.getViewport().Height), new Vector2(100,200), 
+				new Vector2(0,this.ScreenManager.getViewport().Height));
+		this.bodyBlueSlider.setBounds(new Rectangle(0,0,250,30));
+		this.bodyBlueSlider.setScrollMax(255);
+		this.bodyBlueSlider.setScrollValue(255);
+		this.bodyBlueSlider.setHorizontal(true);
+		this.bodyBlueSlider.addScrollListener(new IEventListener<ScrollEventArgs>() {
+			
+			@Override
+			public void onEvent(Object sender, ScrollEventArgs e) {
+				setBodyColor();
+			}
+		});
+
+		this.eyeRedSlider = new Slider();
+		this.eyeRedSlider.setBounds(0, 0, 200, 30);
+		this.eyeRedSlider.setFgColor(new Color(255,50,50,255));
+		super.addGuiControl(this.eyeRedSlider, new Vector2(-100,this.ScreenManager.getViewport().Height), new Vector2(100,300), 
+				new Vector2(0,this.ScreenManager.getViewport().Height));
+		this.eyeRedSlider.setBounds(new Rectangle(0,0,250,30));
+		this.eyeRedSlider.setScrollMax(255);
+		this.eyeRedSlider.setScrollValue(255);
+		this.eyeRedSlider.setHorizontal(true);
+		this.eyeRedSlider.addScrollListener(new IEventListener<ScrollEventArgs>() {
+			
+			@Override
+			public void onEvent(Object sender, ScrollEventArgs e) {
+				setEyeColor();
+			}
+		});
+		this.eyeGreenSlider = new Slider();
+		this.eyeGreenSlider.setBounds(0, 0, 200, 30);
+		this.eyeGreenSlider.setFgColor(new Color(50,255,50,255));
+		super.addGuiControl(this.eyeGreenSlider, new Vector2(-100,this.ScreenManager.getViewport().Height), new Vector2(100,350), 
+				new Vector2(0,this.ScreenManager.getViewport().Height));
+		this.eyeGreenSlider.setBounds(new Rectangle(0,0,250,30));
+		this.eyeGreenSlider.setScrollMax(255);
+		this.eyeGreenSlider.setScrollValue(255);
+		this.eyeGreenSlider.setHorizontal(true);
+		this.eyeGreenSlider.addScrollListener(new IEventListener<ScrollEventArgs>() {
+			
+			@Override
+			public void onEvent(Object sender, ScrollEventArgs e) {
+				setEyeColor();
+			}
+		});
+		this.eyeBlueSlider = new Slider();
+		this.eyeBlueSlider.setBounds(0, 0, 200, 30);
+		this.eyeBlueSlider.setFgColor(new Color(50,50,255,255));
+		super.addGuiControl(this.eyeBlueSlider, new Vector2(-100,this.ScreenManager.getViewport().Height), new Vector2(100,400), 
+				new Vector2(0,this.ScreenManager.getViewport().Height));
+		this.eyeBlueSlider.setBounds(new Rectangle(0,0,250,30));
+		this.eyeBlueSlider.setScrollMax(255);
+		this.eyeBlueSlider.setScrollValue(255);
+		this.eyeBlueSlider.setHorizontal(true);
+		this.eyeBlueSlider.addScrollListener(new IEventListener<ScrollEventArgs>() {
+			
+			@Override
+			public void onEvent(Object sender, ScrollEventArgs e) {
+				setEyeColor();
+			}
+		});
+		
+		this.maneRedSlider = new Slider();
+		this.maneRedSlider.setBounds(0, 0, 200, 30);
+		this.maneRedSlider.setFgColor(new Color(255,50,50,255));
+		super.addGuiControl(this.maneRedSlider, new Vector2(-100,this.ScreenManager.getViewport().Height), new Vector2(100,500), 
+				new Vector2(0,this.ScreenManager.getViewport().Height));
+		this.maneRedSlider.setBounds(new Rectangle(0,0,250,30));
+		this.maneRedSlider.setScrollMax(255);
+		this.maneRedSlider.setScrollValue(255);
+		this.maneRedSlider.setHorizontal(true);
+		this.maneRedSlider.addScrollListener(new IEventListener<ScrollEventArgs>() {
+			
+			@Override
+			public void onEvent(Object sender, ScrollEventArgs e) {
+				setManeColor();
+			}
+		});
+		this.maneGreenSlider = new Slider();
+		this.maneGreenSlider.setBounds(0, 0, 200, 30);
+		this.maneGreenSlider.setFgColor(new Color(50,255,50,255));
+		super.addGuiControl(this.maneGreenSlider, new Vector2(-100,this.ScreenManager.getViewport().Height), new Vector2(100,550), 
+				new Vector2(0,this.ScreenManager.getViewport().Height));
+		this.maneGreenSlider.setBounds(new Rectangle(0,0,250,30));
+		this.maneGreenSlider.setScrollMax(255);
+		this.maneGreenSlider.setScrollValue(255);
+		this.maneGreenSlider.setHorizontal(true);
+		this.maneGreenSlider.addScrollListener(new IEventListener<ScrollEventArgs>() {
+			@Override
+			public void onEvent(Object sender, ScrollEventArgs e) {
+				setManeColor();
+			}
+		});
+		this.maneBlueSlider = new Slider();
+		this.maneBlueSlider.setBounds(0, 0, 200, 30);
+		this.maneBlueSlider.setFgColor(new Color(50,50,255,255));
+		super.addGuiControl(this.maneBlueSlider, new Vector2(-100,this.ScreenManager.getViewport().Height), new Vector2(100,600), 
+				new Vector2(0,this.ScreenManager.getViewport().Height));
+		this.maneBlueSlider.setBounds(new Rectangle(0,0,250,30));
+		this.maneBlueSlider.setScrollMax(255);
+		this.maneBlueSlider.setScrollValue(255);
+		this.maneBlueSlider.setHorizontal(true);
+		this.maneBlueSlider.addScrollListener(new IEventListener<ScrollEventArgs>() {
+			
+			@Override
+			public void onEvent(Object sender, ScrollEventArgs e) {
+				setManeColor();
+			}
+		});
+
+//		this.maneStyleSpinner = new Spinner();
+		
+		this.button2 = new Button();
+		this.button2.setText("Back");
+		this.button2.setBounds(250,710,200,50);
+		super.addGuiControl(button2, new Vector2(this.ScreenManager.getViewport().Width - 250, this.ScreenManager.getViewport().Height), 
+									 new Vector2(this.ScreenManager.getViewport().Width - 250,this.ScreenManager.getViewport().Height-100), 
+									 new Vector2(this.ScreenManager.getViewport().Width - 250, this.ScreenManager.getViewport().Height));
 
 		LookAndFeel feel = contentManager.load("gui.tdict", LookAndFeel.class);
 		feel.setDefaultFont(contentManager.loadFont("arialb20.xml"));
 		ShaderEffect dissabledEffect = contentManager.loadShaderEffect("GrayScale.effect");
 		context = new GUIRenderingContext(this.ScreenManager.getSpriteBatch(), feel, dissabledEffect);
 
-		this.button.addClicked(new IEventListener<EventArgs>() {
-			@Override
-			public void onEvent(Object sender, EventArgs e) {
-				System.out.println("Stuff has been done!");
-			}
-		});
-
 		this.button2.addClicked(new IEventListener<EventArgs>() {
 
 			@Override
 			public void onEvent(Object sender, EventArgs e) {
-				System.out.println("Other stuff has been done!");
+				goBack();
 			}
 		});
 
+	}
+	public void goBack() {
+		this.exitScreen();
+	}
+	
+	protected void setBodyColor() {
+		Color color = new Color(this.bodyRedSlider.getScrollValue(),this.bodyGreenSlider.getScrollValue(),this.bodyBlueSlider.getScrollValue(),255);
+		PonyColorChangeHelper.setBodyColor(color, pony);
+	}
+	protected void setEyeColor() {
+		Color color = new Color(this.eyeRedSlider.getScrollValue(),this.eyeGreenSlider.getScrollValue(),this.eyeBlueSlider.getScrollValue(),255);
+		PonyColorChangeHelper.setEyeColor(color, pony);
+	}
+	protected void setManeColor() {
+		Color color = new Color(this.maneRedSlider.getScrollValue(),this.maneGreenSlider.getScrollValue(),this.maneBlueSlider.getScrollValue(),255);
+		PonyColorChangeHelper.setManeColor(color, pony);
 	}
 
 	protected void showPauseScreen() {
@@ -110,20 +283,13 @@ public class PonyCreatorScreen extends TransitioningGUIScreen {
 	private void addPony() {	
 		this.pony = this.contentManager.load("rdset.animset", AnimationPlayer.class);;
 		this.pony.startAnimation("idle");
-		PonyColorChangeHelper.setBodyColor(Color.Wheat, this.pony);
-		PonyColorChangeHelper.setEyeColor(Color.Chocolate, this.pony);
-		PonyColorChangeHelper.setManeColor(Color.Chocolate, this.pony);
-		TextureDictionary dict = this.contentManager.load("rddict.tdict", TextureDictionary.class);
-		this.pony.setBoneTexture(Bones.EYE.getValue(), dict.extractTextureEntry("TSEYE"));
 	}
 	
 	@Override
 	public void render(GameTime time, SpriteBatch batch) {
+		super.render(time, batch);
 		batch.begin();
 		this.pony.draw(batch, ponyPosition, false, 0, Color.White, ponyScale);
 		batch.end();
-		super.render(time, batch);
 	}
-
-
 }
