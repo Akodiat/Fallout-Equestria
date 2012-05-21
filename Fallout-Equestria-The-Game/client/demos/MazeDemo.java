6d6217bb236cbdb24010f433057b415dd4775699
Akodiat <joakimjo@swipnet.se>
2012-05-21 11:15:52 +0200

    The start of a changeling AI

--- Fallout-Equestria-The-Game/client/demos/MazeDemo.java
+++ Fallout-Equestria-The-Game/client/demos/MazeDemo.java
@@ -2,6 +2,8 @@ package demos;
 
 import org.lwjgl.opengl.Display;
 
+import behavior.Behavior;
+import behavior.ChangelingAIScript;
 import behavior.PlayerScript;
 
 import math.MathHelper;
@@ -48,14 +50,14 @@ public class MazeDemo extends Demo {
 
 	@Override
 	public void update(GameTime time) {
-		
+
 		Display.setTitle("Elapsed Time: " + time.getElapsedTime().getTotalMiliSeconds());
-		
+
 		this.mouse.poll(screenDim);
 		this.keyboard.poll();
 		this.gameWorld.update(time);
 		this.gameWorld.getEntityManager().destoryKilledEntities();
-		
+
 		if(Math.random() < 0.5f) {
 			IEntityDatabase database  = this.gameWorld.getDatabase();
 			if(database.getEntityCount() < 2000) {
@@ -70,7 +72,7 @@ public class MazeDemo extends Demo {
 	private IEntity spawnManlyMan() {
 
 		IEntityManager manager = this.gameWorld.getEntityManager();
-		
+
 		//MANLYMAN
 		AnimationPlayer manPlayer = new AnimationPlayer();
 		Animation walkAnimation = ContentManager.load("manlymanwalk.anim", Animation.class);
@@ -78,22 +80,30 @@ public class MazeDemo extends Demo {
 		Animation explodeAnimation = ContentManager.load("manlymanexplosion.anim", Animation.class);
 		manPlayer.addAnimation("explode", explodeAnimation);
 
-		
+
 		manPlayer.startAnimation("walk");
 		AnimationComp manAniCom = new AnimationComp(manPlayer);
-		
-		
+
+
 		IEntity manEntity = manager.createEntity(ContentManager.loadArchetype("ManlyMan.archetype"));
 		manEntity.addComponent(manAniCom);
 		manEntity.addComponent(new ShadowComp());
 
 		manEntity.getComponent(TransformationComp.class).setRotation((float)(MathHelper.Tau * Math.random()));
+
+		if(Math.random() < 0.3){
+			Behavior behavior = new ChangelingAIScript();
+			behavior.initialize(this.gameWorld.getEntityManager(), this.ContentManager, new SoundManager(this.ContentManager, 1, 1, 1), manEntity); //TODO BAD!!! I guess...
+			behavior.start();
+			manEntity.getComponent(BehaviourComp.class).setBehavior(behavior);
+		}
+
 		manEntity.refresh();
 		//ENDMAN
-		
+
 		placeAtRandomPosition(manEntity);
 		return manEntity;
-		
+
 	}
 
 	@Override
@@ -108,15 +118,15 @@ public class MazeDemo extends Demo {
 	protected void initialize() {
 
 		Display.setVSyncEnabled(false);
-		
-		
+
+
 		scene = ContentManager.load("PerspectiveV5.xml", Scene.class);
 		camera = new Camera2D(scene.getWorldBounds(), screenDim);
 		spriteBatch = new SpriteBatch(screenDim);
 		mouse = new Mouse();
 		keyboard = new Keyboard();
 		SoundManager soundManager = new SoundManager(this.ContentManager,0.1f,1.0f,1.0f);
-		
+
 		this.gameWorld = WorldBuilder.buildGameWorld(camera, scene ,mouse, keyboard, this.ContentManager,soundManager, spriteBatch, true, "Player");
 
 		gameWorld.initialize();
@@ -128,15 +138,15 @@ public class MazeDemo extends Demo {
 		entity.addComponent(new BehaviourComp(new PlayerScript()));
 		entity.addComponent(new ShadowComp());
 		entity.getComponent(TransformationComp.class).setPosition(1000,1000);	
-		
+
 		AnimationPlayer player = this.ContentManager.loadAnimationSet("rdset.animset");
 		AnimationComp comp = new AnimationComp(player);
 		comp.setTint(Color.Green);
-		
+
 		entity.removeComponent(RenderingComp.class);	
 		entity.addComponent(comp);
 		//END OF ANIMATION UGLY SHIT
-		
+
 		entity.addToGroup(CameraControlSystem.GROUP_NAME);
 		entity.refresh();
 
@@ -147,17 +157,17 @@ public class MazeDemo extends Demo {
 			IEntity entity = this.gameWorld.getEntityManager().createEmptyEntity();
 			TransformationComp transComp = new TransformationComp();
 			transComp.setPosition(tNode.getPosition());
-			
+
 			RenderingComp renderComp = new RenderingComp();
 			renderComp.setTexture(tNode.getTexture());
 			renderComp.setSource(tNode.getSrcRectangle());
-			
+
 			entity.addComponent(transComp);
 			entity.addComponent(renderComp);
 			entity.refresh();
 		}
-		
-		
+
+
 	}
 
 	private void placeAtRandomPosition(IEntity entity) {
---
+++ Fallout-Equestria-The-Game/src/behavior/ChangelingAIScript.java
@@ -0,0 +1,90 @@
+package behavior;
+
+import animation.AnimationPlayer;
+import anotations.Editable;
+import math.MathHelper;
+import math.Vector2;
+import components.AnimationComp;
+import components.PhysicsComp;
+import components.RadiationComp;
+import components.TransformationComp;
+import entityFramework.ComponentMapper;
+import entityFramework.IEntity;
+
+import utils.time.GameTime;
+/**
+ * 
+ * @author Joakim Johansson
+ *
+ */
+@Editable
+public class ChangelingAIScript extends Behavior{
+
+	private AnimationComp animComp;
+	private TransformationComp transComp;
+
+	@Editable
+	public float sightRange = 300f;
+
+	@Editable
+	public String targetGroup = "Players";
+
+	@Override
+	public void start() {
+		this.transComp = this.Entity.getComponent(TransformationComp.class);
+		this.animComp = this.Entity.getComponent(AnimationComp.class);
+
+		if(animComp == null) {
+			this.animComp = new AnimationComp(); 
+			this.Entity.addComponent(new AnimationComp()); //TODO Add animComp instead?
+		}
+		if(this.transComp == null) {
+			throw new NullPointerException("transComp");
+		}
+	}
+
+	@Override
+	public void update(GameTime time) {
+		IEntity targetEntity = findNearestTarget(this.Entity.getComponent(TransformationComp.class).getPosition());
+		if(targetEntity != null) {
+			TransformationComp playerTrans = targetEntity.getComponent(TransformationComp.class);
+			if(targetInRange(playerTrans.getPosition())) {
+				copyTargetAppearance(targetEntity);
+			}
+		}
+
+	}
+	private IEntity findNearestTarget(Vector2 position){
+		IEntity nearestTarget = null;
+
+		//		ComponentMapper<TransformationComp> tCM;
+		//		tCM = ComponentMapper.create(NO DATABASE), TransformationComp.class);
+
+		for (IEntity player : this.EntityManager.getEntityGroup(this.targetGroup)) {
+			if(Vector2.distance(position, player.getComponent(TransformationComp.class).getPosition()) < this.sightRange){
+				if (nearestTarget == null ||
+						Vector2.distance(position, player.getComponent(TransformationComp.class).getPosition()) < 
+						Vector2.distance(position, nearestTarget.getComponent(TransformationComp.class).getPosition())){
+					nearestTarget = player;
+				}
+			}
+		}
+		return nearestTarget;
+	}
+
+	private void copyTargetAppearance(IEntity targetEntity) {
+		AnimationPlayer animPlayer = targetEntity.getComponent(AnimationComp.class).getAnimationPlayer().clone();
+		this.Entity.getComponent(AnimationComp.class).setAnimationPlayer(animPlayer);
+	}
+
+	private boolean targetInRange(Vector2 targetPos) {
+		return Vector2.distanceSquared(this.transComp.getPosition(), targetPos) < this.sightRange * this.sightRange;
+	}
+
+
+	@Override
+	public Object clone() {
+		return new ChangelingAIScript();
+	}
+
+}
