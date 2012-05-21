9c27fa37455d42ff39dd0dde4d26f520bea0a6cd
Gustav Alm Rosenblad <agustav@student.chalmers.se>
2012-05-21 20:43:30 +0200

    RDAnimdemo now showcases new pillanimations.

--- Fallout-Equestria-The-Game/client/demos/RDAnimDemo.java
+++ Fallout-Equestria-The-Game/client/demos/RDAnimDemo.java
@@ -101,20 +101,20 @@ public class RDAnimDemo extends Demo {
 		entity.addComponent(new BehaviourComp(new PlayerScript()));
 		entity.addComponent(new ShadowComp());	
 		
-		AnimationPlayer player = ContentManager.load("rdset.animset", AnimationPlayer.class);
-//		player.setBoneTexture(Bones.UPPERMANE, asd);
-//		player.attachAnimationToBone(Bones.EYE.getValue(), ContentManager.load("monocle.anim", Animation.class));
+		AnimationPlayer player = ContentManager.load("pill.animset", AnimationPlayer.class);
+////		player.setBoneTexture(Bones.UPPERMANE, asd);
+////		player.attachAnimationToBone(Bones.EYE.getValue(), ContentManager.load("monocle.anim", Animation.class));
 		player.startAnimation("idle");
 		AnimationComp animComp = new AnimationComp(player);
-		PonyColorChangeHelper.setBodyColor(Color.Wheat, animComp);
-		PonyColorChangeHelper.setEyeColor(Color.Gray, animComp);
-		PonyColorChangeHelper.setManeColor(Color.Brown, animComp);
-		TextureDictionary dict = ContentManager.load("rddict.tdict", TextureDictionary.class);
-		player.setBoneTexture(Bones.EYE.getValue(), dict.extractTextureEntry("TSEYE"));
-		player.setBoneTexture(Bones.UPPERMANE.getValue(), dict.extractTextureEntry("TSUPPERMANE"));
-		player.setBoneTexture(Bones.LOWERMANE.getValue(), dict.extractTextureEntry("TSLOWERMANE"));
-		player.setBoneTexture(Bones.UPPERTAIL.getValue(), dict.extractTextureEntry("TSUPPERTAIL"));
-		player.setBoneTexture(Bones.LOWERTAIL.getValue(), dict.extractTextureEntry("TSLOWERTAIL"));
+//		PonyColorChangeHelper.setBodyColor(Color.Wheat, animComp);
+//		PonyColorChangeHelper.setEyeColor(Color.Gray, animComp);
+//		PonyColorChangeHelper.setManeColor(Color.Brown, animComp);
+//		TextureDictionary dict = ContentManager.load("rddict.tdict", TextureDictionary.class);
+//		player.setBoneTexture(Bones.EYE.getValue(), dict.extractTextureEntry("TSEYE"));
+//		player.setBoneTexture(Bones.UPPERMANE.getValue(), dict.extractTextureEntry("TSUPPERMANE"));
+//		player.setBoneTexture(Bones.LOWERMANE.getValue(), dict.extractTextureEntry("TSLOWERMANE"));
+//		player.setBoneTexture(Bones.UPPERTAIL.getValue(), dict.extractTextureEntry("TSUPPERTAIL"));
+//		player.setBoneTexture(Bones.LOWERTAIL.getValue(), dict.extractTextureEntry("TSLOWERTAIL"));
 		entity.addComponent(animComp);
 		//END OF ANIMATION UGLY SHIT
 		entity.getComponent(TransformationComp.class).setPosition(1000, 1000);
