df669dc7097a2eb1e6c47ecc05148d051b1b52af
Akodiat <joakimjo@swipnet.se>
2012-05-21 19:13:08 +0200

    Level1 file

---
+++ Fallout-Equestria-The-Game/src/screenCore/Level1.java
@@ -0,0 +1,13 @@
+package screenCore;
+
+import utils.time.TimeSpan;
+
+public class Level1 extends Level{
+
+	public Level1(boolean popup, TimeSpan transOnTime, TimeSpan transOffTime,
+			String level) {
+		super(popup, transOnTime, transOffTime, level);
+	}
+	
+
+}
